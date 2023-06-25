4acc6a556891e32971ee1892941055c799196674
Metaldetect <stas.klymenko@icloud.com>
2023-06-25 19:50:37 +0200

    fix bugs and add button "to top"

--- js/modal.js
+++ js/modal.js
@@ -1,28 +1,44 @@
 (() => {
+  const backdrop = document.getElementById("backdrop");
   const mobileMenu = document.querySelector(".js-menu-container");
   const openMenuBtn = document.querySelector(".js-open-menu");
   const closeMenuBtn = document.querySelector(".js-close-menu");
+  const button = document.getElementById("form-btn");
+  const form = document.getElementById("form");
 
   const toggleMenu = () => {
     const isMenuOpen =
       openMenuBtn.getAttribute("aria-expanded") === "true" || false;
     openMenuBtn.setAttribute("aria-expanded", !isMenuOpen);
     mobileMenu.classList.toggle("is-open");
-
-    const scrollLockMethod = !isMenuOpen
-      ? "disableBodyScroll"
-      : "enableBodyScroll";
-    bodyScrollLock[scrollLockMethod](document.body);
+    backdrop.classList.toggle("active");
   };
 
   openMenuBtn.addEventListener("click", toggleMenu);
   closeMenuBtn.addEventListener("click", toggleMenu);
 
-  // Close the mobile menu on wider screens if the device orientation changes
-  window.matchMedia("(min-width: 768px)").addEventListener("change", (e) => {
-    if (!e.matches) return;
-    mobileMenu.classList.remove("is-open");
-    openMenuBtn.setAttribute("aria-expanded", false);
-    bodyScrollLock.enableBodyScroll(document.body);
+  button.addEventListener("click", function () {
+    form.scrollIntoView({ behavior: "smooth" });
+  });
+
+  const anotherButton1 = document.getElementById("form-btn-1");
+  anotherButton1.addEventListener("click", function () {
+    form.scrollIntoView({ behavior: "smooth" });
+  });
+
+  const anotherButton2 = document.getElementById("form-btn-2");
+  anotherButton2.addEventListener("click", function () {
+    form.scrollIntoView({ behavior: "smooth" });
+  });
+
+  const anotherButton3 = document.getElementById("form-btn-3");
+  anotherButton3.addEventListener("click", function () {
+    form.scrollIntoView({ behavior: "smooth" });
+  });
+
+  const anotherButton4 = document.getElementById("form-btn-4");
+  anotherButton4.addEventListener("click", function () {
+    form.scrollIntoView({ behavior: "smooth" });
+    toggleMenu();
   });
 })();
---
+++ js/totop.js
@@ -0,0 +1,8 @@
+const toTopButton = document.getElementById("to-top-btn");
+
+toTopButton.addEventListener("click", function () {
+  window.scrollTo({
+    top: 0,
+    behavior: "smooth",
+  });
+});
